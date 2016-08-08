eb68640fec3802d5828c20039cfff1994b1b649e
marmanold <michael@rnold.info>
2016-08-07 21:15:43 -0500

    Added guts to get some of the Sunday in the calendar.  Mostly through Easter.

--- lib/Date/Lectionary.pm
+++ lib/Date/Lectionary.pm
@@ -8,7 +8,9 @@ use Moose;
 use Carp;
 use Try::Tiny;
 use Time::Piece;
+use Time::Seconds;
 use Date::Advent;
+use Date::Easter;
 use Date::Lectionary::Time qw(nextSunday prevSunday);
 use Date::Lectionary::Reading;
 use namespace::autoclean;
@@ -83,10 +85,11 @@ sub BUILD {
 	my $self = shift;
 
 	my $advent = _buildAdvent($self->date);
+	my $easter = _buildEaster($advent->firstSunday->year+1);
 
 	$self->_setYear(_determineYear($advent->firstSunday->year));
 
-	$self->_setDay(_determineDay($self->date));
+	$self->_setDay(_determineDay($self->date, $advent, $easter));
 
 	$self->_setReadings(_buildReadings());
 }
@@ -103,6 +106,242 @@ sub _buildAdvent {
 	catch{
 		confess "Could not calculate Advent for the given date [". $date->ymd ."].";
 	}
+}
+
+sub _buildEaster {
+	my $easterYear = shift;
+
+	my $easter = undef;
+
+	try{
+		my ($easterMonth, $easterDay) = easter($easterYear);
+		$easter = Time::Piece->strptime($easterYear."-".$easterMonth."-".$easterDay, "%Y-%m-%d");
+		return $easter;
+	}
+	catch{
+		confess "Could not calculate Easter for the year [". $easterYear ."]";
+	}
+}
+
+=head2 _buildAshWednesday
+
+	Ash Wednesday is the start of Lent.  It occurs 46 days before Easter for the given year.
+
+=cut
+sub _determineAshWednesday {
+	my $easter = shift;
+
+	my $ashWednesday = undef;
+
+	try{
+		my $secondsToSubtract = 46 * ONE_DAY;
+		$ashWednesday = $easter - $secondsToSubtract;
+		return $ashWednesday;
+	}
+	catch{
+		confess "Could not calculate Ash Wednesday for Easter [". $easter->ymd ."].";
+	}
+}
+
+sub _determineHolyWeek {
+	my $date = shift;
+	my $easter = shift;
+
+	my $dateMark = $easter - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Holy Saturday";
+	}
+
+	$dateMark = $dateMark - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Good Friday";
+	}
+
+	$dateMark = $dateMark - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Maundy Thursday";
+	}
+
+	$dateMark = $dateMark - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Wednesday in Holy Week";
+	}
+
+	$dateMark = $dateMark - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Tuesday in Holy Week";
+	}
+
+	$dateMark = $dateMark - ONE_DAY;
+	if ($date == $dateMark) {
+		return "Monday in Holy Week";
+	}
+
+	return undef;
+}
+
+
+sub _buildFixedDays {
+	my $date = shift;
+
+	#Fixed holidays in January
+	if($date->mon == 1) {
+
+	}
+	#Fixed holidays in February
+	elsif($date->mon == 2) {
+
+	}
+	#Fixed holidays in March
+	elsif($date->mon == 3) {
+
+	}
+	#Fixed holidays in April
+	elsif($date->mon == 4) {
+
+	}
+	#Fixed holidays in May
+	elsif($date->mon == 5) {
+
+	}
+	#Fixed holidays in June
+	elsif($date->mon == 6) {
+
+	}
+	#Fixed holidays in July
+	elsif($date->mon == 7) {
+
+	}
+	#Fixed holidays in August
+	elsif($date->mon == 8) {
+
+	}
+	#Fixed holidays in September
+	elsif($date->mon == 9) {
+
+	}
+	#Fixed holidays in October
+	elsif($date->mon == 10) {
+
+	}
+	#Fixed holidays in November
+	elsif($date->mon == 11) {
+
+	}
+	#Fixed holidays in December
+	elsif($date->mon == 12) {
+		if ($date->mday == 25) { return "Christmas Day";}
+	}
+	else {
+		confess "Date [". $date->ymd . "] is not a known or valid date.";
+	}
+}
+
+sub _determineChristmasEpiphany {
+	my $date = shift;
+
+	my $advent = shift;
+
+	#Is the date in Christmastide?
+	my $dateMarker = nextSunday($advent->fourthSunday);
+	if ($date == $dateMarker) {
+		return "The First Sunday of Christmas";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Second Sunday of Christmas";
+	}
+
+	#Is the date in Epiphany?
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The First Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Second Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Third Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Fourth Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Fifth Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Sixth Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Seventh Sunday of Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Second to Last Sunday after Epiphany";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Last Sunday after Epiphany";
+	}
+
+	croak "There are no further Sundays of Christmastide or Epiphany.";
+}
+
+sub _determineLentHolyWeek {
+	my $date = shift;
+	my $ashWednesday = shift;
+
+	my $dateMarker = nextSunday($ashWednesday);
+	if ($date == $dateMarker) {
+		return "The First Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Second Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Third Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Fourth Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Fifth Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "The Fifth Sunday in Lent";
+	}
+
+	$dateMarker = nextSunday($dateMarker);
+	if ($date == $dateMarker) {
+		return "Palm Sunday";
+	}
+
+	croak "There are no further Sundays in Lent";
 }
 
 sub _determineYear {
@@ -132,7 +371,55 @@ sub _buildReadings {
 sub _determineDay {
 	my $date = shift;
 
-	return 'Christmas';
+	my $advent = shift;
+	my $easter = shift;
+
+	#Is the date in Advent?
+	if ($date == $advent->firstSunday) {
+		return "The First Sunday in Advent";
+	}
+	elsif ($date == $advent->secondSunday) {
+		return "The Second Sunday in Advent";
+	}
+	elsif ($date == $advent->thirdSunday) {
+		return "The Third Sunday in Advent";
+	}
+	elsif ($date == $advent->fourthSunday) {
+		return "The Fourth Sunday in Advent";
+	}
+
+	#Is the date Easter Sunday?
+	if ($date == $easter) {
+		return "Easter Day";
+	}
+
+	#Determine when Ash Wednesday is
+	my $ashWednesday = _determineAshWednesday($easter);
+	if ($date == $ashWednesday) {
+		return "Ash Wednesday";
+	}
+
+	#Holy Week
+	my $holyWeekDay = _determineHolyWeek($date, $easter);
+	if ($holyWeekDay) {return $holyWeekDay;}
+
+	#Fixed celebrations
+	my $fixedDay = _buildFixedDays($date);
+	if ($fixedDay) {return $fixedDay;}
+
+	if($date->wday!=1) {
+		my $nextSunday = nextSunday($date);
+		$date = $nextSunday;
+	}
+
+	#Sundays of the Liturgical Year
+	if($date < $ashWednesday) {
+		return _determineChristmasEpiphany($date, $advent);
+	}
+
+	if($date < $easter) {
+		return _determineLent($date, $ashWednesday);
+	}
 }
 
 =head1 AUTHOR
---
+++ t/02-day.t
@@ -0,0 +1,37 @@
+#!perl -T
+use v5.22;
+use strict;
+use warnings;
+use Test::More tests=>4;
+use Test::Exception;
+
+use Time::Piece;
+use Date::Lectionary;
+
+my $christmas = Date::Lectionary->new('date'=>Time::Piece->strptime("2016-12-25", "%Y-%m-%d"));
+is(
+	$christmas->day, 
+	'Christmas Day', 
+	'Ensure that December 25, 2016 is Christmas Day'
+);
+
+my $ashWednesday = Date::Lectionary->new('date'=>Time::Piece->strptime("2016-02-10", "%Y-%m-%d"));
+is(
+	$ashWednesday->day,
+	'Ash Wednesday', 
+	'Ensure that February 10, 2016 is Ash Wednesday'
+);
+
+my $easter = Date::Lectionary->new('date'=>Time::Piece->strptime("2016-03-27", "%Y-%m-%d"));
+is(
+	$easter->day,
+	'Easter Day', 
+	'Ensure that March 27, 2016 is Easter Day'
+);
+
+my $holySat = Date::Lectionary->new('date'=>Time::Piece->strptime("2016-03-26", "%Y-%m-%d"));
+is(
+	$holySat->day,
+	'Holy Saturday', 
+	'Ensure that March 26, 2016 is Holy Saturday'
+);
